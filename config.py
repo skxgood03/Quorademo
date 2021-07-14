4a503a4c36c6649971a47f71098598c2b0436559
shikexuan <2399745557@qq.com>
2021-07-14 16:37:45 +0800

    发布问答使用markdown插件，可給文章插入图片

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 DEBUG=True
-
+WTF_CSRF_CHECK_DEFAULT = False
 SECRET_KEY = os.urandom(24)
 
 DB_URL='mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format('root','skx','127.0.0.1','3306','project_demo')
--- houpu.py
+++ houpu.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, request, flash, redirect, url_for,session
+
+import os
+from datetime import datetime
+
+from flask import Flask, render_template, request, flash, redirect, url_for, session, Response, jsonify
+
+# from requests import Response
+from flaskext.markdown import Markdown
+from flask_wtf.csrf  import CSRFProtect
+
 from models import User,Question,Answer
 from exts import db
 from functools import wraps
 from sqlalchemy import or_
+
 import config
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+Markdown(app)
 app.config.from_object(config)
 app.secret_key='itheima'
 db.init_app(app)
@@ -148,8 +160,40 @@ def my_context_processor():
         if user:
             return {'user':user}
     return {}
-
-
+@app.route('/upload/', methods=['GET','POST'])
+@login_astrict
+@csrf.exempt
+def upload():
+    file = request.files.get('editormd-image-file')
+    if not file:
+        res = {
+            'success': 0,
+            'message': '上传失败'
+        }
+    else:
+        ex = os.path.splitext(file.filename)[1]
+        filename = datetime.now().strftime('%Y%m%d%H%M%S') + ex
+        bodypic_path = 'static/asset/' + filename
+        file.save(bodypic_path)
+        res = {
+            'success': 1,
+            'message': '上传成功',
+            'url': url_for('.image', name=filename)
+        }
+    return jsonify(res)
+# 隐藏后端静态文件目录的效果
+@app.route('/image/<name>', methods=['GET'])
+@login_astrict
+@csrf.exempt
+def image(name):
+    current_path = os.path.abspath(os.path.dirname(__file__))
+    father_path = os.path.abspath(os.path.dirname(current_path) + os.path.sep)
+    filedir = os.path.join(father_path,'project_demo\\static\\asset\\')
+    filepath = os.path.join(filedir)
+    with open(os.path.join(filepath, name), 'rb') as f:
+        resp=Response(f.read(), mimetype="image/jpeg")
+        print(resp)
+    return resp
 
 if __name__ == '__main__':
     app.run()
